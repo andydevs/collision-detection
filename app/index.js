21d6f526d8b5f30de8ffa4d3177ed2c3e216d2ed
andydevs <akanshul97@gmail.com>
2021-05-02 02:12:18 -0400

    Actual collision momentum exchange!

--- app/index.js
+++ app/index.js
@@ -7,6 +7,7 @@
 import './style/main.scss'
 import Vector from './vector'
 
+const PAUSE_ON_COLLISION = false
 const DEBUG_COLLISIONS = true
 
 // Get canvas and drawcontext
@@ -33,9 +34,10 @@ function drawRay(p, d, l, color, width=1) {
     drawLine(p, s, color, width)
 }
 
-function drawCircle(pos, rad) {
+function drawCircle(pos, rad, width=1) {
     let transformed = centered(pos)
     ctx.strokeStyle = '#000000'
+    ctx.lineWidth = width
     ctx.beginPath()
     ctx.arc(transformed.x, transformed.y, rad, 0, 2*Math.PI)
     ctx.stroke()
@@ -58,8 +60,16 @@ class Ball {
 }
 
 function boundaryCollision(ball) {
-    if (Math.abs(ball.pos.y) > canvas.height/2 - ball.rad) { ball.vel.y *= -1 }
-    if (Math.abs(ball.pos.x) > canvas.width/2 - ball.rad) { ball.vel.x *= -1 }
+    let collided = false
+    if (Math.abs(ball.pos.y) > canvas.height/2 - ball.rad) {
+        ball.vel.y *= -1
+        collided ||= true
+    }
+    if (Math.abs(ball.pos.x) > canvas.width/2 - ball.rad) {
+        ball.vel.x *= -1
+        collided ||= true
+    }
+    return collided
 }
 
 function ballCollision(a, b) {
@@ -67,27 +77,45 @@ function ballCollision(a, b) {
     let d = b.pos.sub(a.pos)
 
     // If distance is greater than both radii
-    if (d.magnitude() < a.rad + b.rad) {
-        // First find the collision normal
-        let n = d.unit()
+    if (d.magnitude() < (a.rad + b.rad)) {
+        // First find the collision normals
+        let na = d.unit()
+        let nb = na.scale(-1)
 
-        // Find component of velocity parallel to normal (u)
-        // and component of velocity perpendicular to normal (v)
-        let u = n.scale(a.vel.dot(n))
-        let v = a.vel.sub(u)
+        // Find projections of velocity on normal
+        let ua = na.scale(a.vel.dot(na))
+        let ub = nb.scale(b.vel.dot(nb))
+
+        // Get other components of velocity off normal
+        let wa = a.vel.sub(ua)
+        let wb = b.vel.sub(ub)
+
+        // Momentum exchange
+        let p = ua.magnitude() + ub.magnitude()
+        let va = nb.scale(p/2).add(wa)
+        let vb = na.scale(p/2).add(wb)
 
         // Debug draw ray
         if (DEBUG_COLLISIONS) {
+            // Collision normal line
             drawLine(a.pos, b.pos, '#000000')
-            drawRay(a.pos, v.unit(), a.rad, '#0000ff')
-            drawRay(a.pos, a.vel.unit(), a.rad, '#ff0000')
-            drawRay(a.pos, u.scale(-1).add(v).unit(), a.rad, '#009900', 2)
+
+            // Ball A info rays
+            drawRay(a.pos, a.vel.unit(), a.rad, '#ff0000') // Velocity
+            drawRay(a.pos, wa.unit(), a.rad, '#0000ff') // Reflection line
+            drawRay(a.pos, va.unit(), a.rad, '#00ff00', 3) // New Velocity
+
+            // Ball B info rays
+            drawRay(b.pos, b.vel.unit(), b.rad, '#ff0000') // Velocity
+            drawRay(b.pos, wb.unit(), b.rad, '#0000ff') // Reflection line
+            drawRay(b.pos, vb.unit(), b.rad, '#00ff00', 3) // New Velocity
         }
 
-        // New velocity is the negative parallel + perpendicular
-        a.vel = u.scale(-1).add(v)
+        // Set velocities
+        a.vel = va
+        b.vel = vb
 
-        // Return true if the ball collided
+        // Return true because the ball collided
         return true
     }
 
@@ -150,9 +178,8 @@ function animate() {
 
         // Ball collision detection
         for (let [a, b] of pairs) {
-            let acolb = ballCollision(a,b)
-            let bcola = ballCollision(b,a)
-            if (DEBUG_COLLISIONS && (acolb || bcola)) {
+            let collision = ballCollision(a,b)
+            if (PAUSE_ON_COLLISION && collision) {
                 wait = 100
             }
         }
